4dc38938a3068d39515cabf03ba6c1eb63b5fba0
deepdass <deepdas26052008@gmail.com>
2026-03-17 00:55:13 +0530

    basic movement

---
+++ Scripts/Player_camerarig.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+var Sensitivity = .001
+
+@onready var camera_3d: Camera3D = $Camera3D
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		self.rotate_y(event.relative.x * Sensitivity)
+		camera_3d.rotate_x(event.relative.y * Sensitivity)
+		
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-60), deg_to_rad(90))
