ac926e5586ef7286e43adeba0dd1f6ef7eed751a
deepdass <deepdas26052008@gmail.com>
2026-03-24 15:10:07 +0530

    fix cam

--- Scripts/managers/fixed_cam.gd
+++ Scripts/managers/fixed_cam.gd
@@ -1,16 +1,21 @@
 extends Camera3D
 
 @export var follow_player = false
+@export var look_speed : float = 4
+
 var player : Player = null
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if follow_player and player:
-		look_at(player.global_position)
+		var direction = (player.global_position + Vector3(0, 1.5, 0) - global_position).normalized()
+		var target_basis = Basis().looking_at(direction, Vector3.UP)
+
+		global_transform.basis = global_transform.basis.slerp(target_basis, look_speed * delta)
 
 
 func _on_trigger_body_entered(body: Node3D) -> void:
 	if body is Player:
 		player = body
-		##for camera : Camera3D in get_tree().get_nodes_in_group("camera"):
+		##for camera : Camera3D in get_tree().get_nodes_in_group("camera"): 
 			##camera.current = false
 		current = true
--- Scripts/managers/gamemode.gd
+++ Scripts/managers/gamemode.gd
@@ -7,3 +7,6 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ESC"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
